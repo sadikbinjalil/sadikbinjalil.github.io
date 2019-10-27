fb80fa8576efdf72f9300790181295090e311444
Sadik Bin Jalil <53493191+sadikbinjalil@users.noreply.github.com>
2019-10-27 22:05:45 +0600

    Portfolio Website

---
+++ vendors/style-switcher/switcher-active.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$('#toggle-switcher').click(function(){if($(this).hasClass('open')){$(this).removeClass('open');$('#switch-style').animate({'left':'-200px'});}else{$(this).addClass('open');$('#switch-style').animate({'left':'0'});}});});
